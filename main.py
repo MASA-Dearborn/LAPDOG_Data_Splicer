3bfa6068eb22c49373b4239f88f362e3b3f35d1e
Kai Leung <kjleung@umich.edu>
2022-03-18 16:02:57 -0400

    desegmentation script

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+import os
 
-if __name__ == "__main__":
-    pass
+def desegment(message_name : str):
+    directory = './log/' + message_name
+     
+    with open(os.path.abspath('./log/' + message_name + '/' + message_name + '.txt'), 'w') as writeFile: #change .txt to .log after debug
+
+        for filename in os.listdir(directory):
+            f = os.path.join(directory, filename)
+
+            if os.path.isfile(f):
+                readFile = open(f,'r')
+                writeFile.write(readFile.read() + '\n') 
+
+if __name__ == '__main__':
+
+    for folder in os.listdir('./log/'):
+        desegment(folder)
+
